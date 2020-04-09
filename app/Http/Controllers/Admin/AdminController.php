ce64f7609ab1368860d3a9c8fe6712ed4d09c3ae
mdsharafat <sharafat.sohan047@gmail.com>
2020-04-09 11:46:31 +0600

    admin dashboard view changed

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Subscribe;
+use App\Comment;
+use App\Contact;
 
 class AdminController extends Controller
 {
@@ -14,6 +17,10 @@ class AdminController extends Controller
 
     public function index()
     {
-        return view('admin.dashboard');
+        $totalSubscription = Subscribe::all()->count();
+        $totalUnreadContactMessage = Contact::where('is_archived', 0)->get()->count();
+        $totalReadContactMessage = Contact::where('is_archived', 1)->get()->count();
+        $totalPendingComment = Comment::where('is_pending', 1)->get()->count();
+        return view('admin.dashboard', compact('totalSubscription', 'totalUnreadContactMessage', 'totalReadContactMessage', 'totalPendingComment'));
     }
 }
